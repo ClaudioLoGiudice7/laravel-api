b7c17a30f5b2a0e939aa96d29439dcba5ea4a07a
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-04-12 00:36:40 +0200

    entering "Project" model creation

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker; 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 40; $i++) { 
+            
+        }
+    }
+}
