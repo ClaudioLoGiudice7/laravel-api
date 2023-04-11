cacc991f2b718dfeb27cdd236b8d010c44caab1c
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-04-12 00:54:40 +0200

    seeder launched twice by mistake (but i think it doesn't matter: database is more populated)

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Faker\Generator as Faker; 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,8 +16,15 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 40; $i++) { 
-            
+        for ($i=0; $i < 50; $i++) { 
+            $project = new Project;
+
+            $project->name = $faker->catchPhrase();
+            $project->description = $faker->paragraph(2, false );
+            $project->technology_used = $faker->words(1, true);
+            $project->start_date = $faker->dateTime();
+
+            $project->save();           
         }
     }
 }
